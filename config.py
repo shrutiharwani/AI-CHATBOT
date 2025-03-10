49b9cb2b092f00ff8f5238074eb587f5213bf28b
Shruti Harwani <81618404+shrutiharwani@users.noreply.github.com>
2025-03-10 20:11:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import secrets
 
 class Config:
     SECRET_KEY = secrets.token_hex(16) # For Flask security
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:shru6@localhost:5432/AI_CHAT_BOT"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost:5432/YOUR_DATABASE"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     GEMINI_API_KEY = "Your API Key"  # Replace with your actual API key
     JWT_SECRET_KEY = "Your JWT Key"
