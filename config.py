b628df1c75947c7c8255d8d449b3ea50a8142315
Shruti Harwani <81618404+shrutiharwani@users.noreply.github.com>
2025-03-10 20:10:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config:
     SECRET_KEY = secrets.token_hex(16) # For Flask security
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:shru6@localhost:5432/AI_CHAT_BOT"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    GEMINI_API_KEY = "AIzaSyDBSPAci-nRScxYm-rQ8MJbzTJEBqhCU3A"  # Replace with your actual API key
-    JWT_SECRET_KEY = "f8e606b3d0685d3dc05a141711e06044ceaf2643a1c7b1afd946877bf7b91a61"
+    GEMINI_API_KEY = "Your API Key"  # Replace with your actual API key
+    JWT_SECRET_KEY = "Your JWT Key"
     JWT_ACCESS_TOKEN_EXPIRES = 3600  #  Tokens expire in 1 hour
 
 
